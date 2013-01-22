6c1f9ac850dd674ed3f06c108bcf36ad2c0f6c75
Phil Johnston <johnstonphilip@gmail.com>
2013-01-22 13:11:54 -0500

    AQ Resizer with retina check and display
    AQ Resizer with retina check using a cookie. The cookie is set based on
    screensize. Images are resized and shown accordingly.

--- aq_resizer.php
+++ aq_resizer.php
@@ -1,114 +1,42 @@
 <?php
+function get_pixelratio(){
+    if( isset($_COOKIE["pixel_ratio"]) ){
+        $pixel_ratio = $_COOKIE["pixel_ratio"];
+        if( $pixel_ratio >= 2 ){
+           // echo "Is HiRes Device";
+        	
+			/**
+			* Include AQ Resizer
+			*/
+			require( get_template_directory() . '/inc/aq_resizer/aq_resizer-2x.php' );
 
-/**
-* Title		: Aqua Resizer
-* Description	: Resizes WordPress images on the fly
-* Version	: 1.1.4
-* Author	: Syamil MJ
-* Author URI	: http://aquagraphite.com
-* License	: WTFPL - http://sam.zoy.org/wtfpl/
-* Documentation	: https://github.com/sy4mil/Aqua-Resizer/
-*
-* @param string $url - (required) must be uploaded using wp media uploader
-* @param int $width - (required)
-* @param int $height - (optional)
-* @param bool $crop - (optional) default to soft crop
-* @param bool $single - (optional) returns an array if false
-* @uses wp_upload_dir()
-* @uses image_resize_dimensions()
-* @uses image_resize()
-*
-* @return str|array
-*/
-
-function aq_resize( $url, $width, $height = null, $crop = null, $single = true ) {
-	
-	//retina check - if the wp-retina-2x plugin is installed, follow suit and double the size of images
-	if (  in_array( 'wp-retina-2x/wp-retina-2x.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) { 
-		$width = $width * 2;
-		$height = $height * 2;
-	}
-	
-	//validate inputs
-	if(!$url OR !$width ) return false;
-	
-	//define upload path & dir
-	$upload_info = wp_upload_dir();
-	$upload_dir = $upload_info['basedir'];
-	$upload_url = $upload_info['baseurl'];
-	
-	//check if $img_url is local
-	if(strpos( $url, $upload_url ) === false) return false;
-	
-	//define path of image
-	$rel_path = str_replace( $upload_url, '', $url);
-	$img_path = $upload_dir . $rel_path;
-	
-	//check if img path exists, and is an image indeed
-	if( !file_exists($img_path) OR !getimagesize($img_path) ) return false;
-	
-	//get image info
-	$info = pathinfo($img_path);
-	$ext = $info['extension'];
-	list($orig_w,$orig_h) = getimagesize($img_path);
-	
-	//get image size after cropping
-	$dims = image_resize_dimensions($orig_w, $orig_h, $width, $height, $crop);
-	$dst_w = $dims[4];
-	$dst_h = $dims[5];
-	
-	//use this to check if cropped image already exists, so we can return that instead
-	$suffix = "{$dst_w}x{$dst_h}";
-	$dst_rel_path = str_replace( '.'.$ext, '', $rel_path);
-	$destfilename = "{$upload_dir}{$dst_rel_path}-{$suffix}.{$ext}";
-	
-	//if orig size is smaller
-	if($width >= $orig_w) {
-		
-		if(!$dst_h) :
-			//can't resize, so return original url
-			$img_url = $url;
-			$dst_w = $orig_w;
-			$dst_h = $orig_h;
+		}else{
+            //echo "Is NormalRes Device";
 			
-		else :
-			//else check if cache exists
-			if(file_exists($destfilename) && getimagesize($destfilename)) {
-				$img_url = "{$upload_url}{$dst_rel_path}-{$suffix}.{$ext}";
-			} 
-			//else resize and return the new resized image url
-			else {
-				$resized_img_path = image_resize( $img_path, $width, $height, $crop );
-				$resized_rel_path = str_replace( $upload_dir, '', $resized_img_path);
-				$img_url = $upload_url . $resized_rel_path;
-			}
-			
-		endif;
-		
-	}
-	//else check if cache exists
-	elseif(file_exists($destfilename) && getimagesize($destfilename)) {
-		$img_url = "{$upload_url}{$dst_rel_path}-{$suffix}.{$ext}";
-	} 
-	//else, we resize the image and return the new resized image url
-	else {
-		$resized_img_path = image_resize( $img_path, $width, $height, $crop );
-		$resized_rel_path = str_replace( $upload_dir, '', $resized_img_path);
-		$img_url = $upload_url . $resized_rel_path;
-	}
-	
-	//return the output
-	if($single) {
-		//str return
-		$image = $img_url;
-	} else {
-		//array return
-		$image = array (
-			0 => $img_url,
-			1 => $dst_w,
-			2 => $dst_h
-		);
-	}
-	
-	return $image;
-}
+			/**
+			* Include AQ Resizer
+			*/
+			require( get_template_directory() . '/inc/aq_resizer/aq_resizer-1x.php' );
+        }
+    }else{
+		require( get_template_directory() . '/inc/aq_resizer/aq_resizer-1x.php' );
+?>
+    <script language="javascript">
+        writeCookie();
+        function writeCookie()
+        {
+            the_cookie = document.cookie;
+            if( the_cookie ){
+                if( window.devicePixelRatio >= 2 ){
+                    the_cookie = "pixel_ratio="+window.devicePixelRatio+";"+the_cookie;
+                    document.cookie = the_cookie;
+                    location = '<?php $_SERVER['PHP_SELF'] ?>';
+                }
+            }
+        }
+    </script>
+<?php
+    }//isset($_COOKIE["pixel_ratio"]) 
+}//get_pixelratio
+add_action( 'wp_enqueue_scripts', 'get_pixelratio' );
+?>
